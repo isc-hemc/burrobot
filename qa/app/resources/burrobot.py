a0ea94fd0ae2048a5ee3ccec3cf7859307a70a29
David Martinez <davestringm@gmail.com>
2021-01-12 23:31:33 -0600

    modify the endpoint and enabling automatic responses to fb messenger

--- qa/app/resources/burrobot.py
+++ qa/app/resources/burrobot.py
@@ -34,8 +34,16 @@ class BurrobotResource(Resource):
         tokenizer=os.environ.get("BERT_SQUAD_TOKENIZER"),
     )
 
+    def get(self):
+        """GET Request."""
+        verify_token = request.args.get("hub.verify_token")
+        if verify_token == os.environ.get("WEBHOOK_TOKEN"):
+            return int(request.args.get("hub.challenge"))
+
+        return "Unable to authorise."
+
     def post(self) -> Response:
-        """GET Request.
+        """POST Request.
 
         Makes a HTTP request to retrieve a context and perform an evaluation
         using the `question-answer` pipeline.
@@ -52,21 +60,12 @@ class BurrobotResource(Resource):
         """
         payload = request.get_json()
 
-        if not payload:
-            return CustomResponse(
-                message="Empty Payload: JSON content expected."
-            ).error()
-
-        try:
-            context_id, question = itemgetter("context_id", "question")(
-                payload
-            )
-        except KeyError:
-            return CustomResponse(
-                message="context_id or question keys required."
-            ).error()
+        message = payload["entry"][0]["messaging"][0]["message"]
+        sender_id = payload["entry"][0]["messaging"][0]["sender"]["id"]
 
         cms_api = os.environ.get("CMS_API_HOST")
+
+        context_id = 3
 
         try:
             res = http_request.get(f"{cms_api}/context/{context_id}").json()
@@ -77,6 +76,56 @@ class BurrobotResource(Resource):
 
         context = res["data"]["description"]
 
-        answer = self.qa(context=context, question=question)
+        if message["text"]:
+            answer = self.qa(context=context, question=message["text"])
 
-        return CustomResponse(message="Burrobot says:", data=answer).success()
+            response_body = {
+                "messaging_type": "RESPONSE",
+                "recipient": {"id": sender_id},
+                "message": {"text": answer["answer"]},
+            }
+
+            fb_access_token = os.environ.get("FACEBOOK_PAGE_TOKEN")
+            fb_host = (
+                "https://graph.facebook.com/v9.0/me/messages?access_token="
+            )
+            response = http_request.post(
+                f"{fb_host}{fb_access_token}", json=response_body
+            )
+
+            return response.json()
+
+        return CustomResponse(
+            message="Empty Payload: JSON content expected."
+        ).error()
+
+        # if not payload:
+        #     return CustomResponse(
+        #         message="Empty Payload: JSON content expected."
+        #     ).error()
+
+        # try:
+        #     context_id, question = itemgetter("context_id", "question")(
+        #         payload
+        #     )
+        # except KeyError:
+        #     return CustomResponse(
+        #         message="context_id or question keys required."
+        #     ).error()
+
+        # cms_api = os.environ.get("CMS_API_HOST")
+
+        # try:
+        #     res = http_request.get(f"{cms_api}/context/{context_id}").json()
+        # except Exception:
+        #     return CustomResponse(
+        #         message="Unable to reach the CMS API."
+        #     ).error(404)
+
+        # context = res["data"]["description"]
+
+        # answer = self.qa(context=context, question=question)
+
+        # return CustomResponse(
+        #       message="Burrobot says:", data=answer
+        #   ).success()
